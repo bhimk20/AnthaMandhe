e451986b4543e628a259563ae83b0a1777197a94
bhimk20 <94125747+bhimk20@users.noreply.github.com>
2021-12-17 08:06:11 +0530

    Comment

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function countDown() {
   const days = 17 - new Date().getDate() - 1;
   const hours = 23 - new Date().getHours(); 
   const mins = 59 - new Date().getMinutes(); 
-  const secs = 59 - new Date().getSeconds(); 
+ //const secs = 59 - new Date().getSeconds(); 
 
-  noti.textContent = "Terrorist birthday in " + days + "d " + hours + "h " + mins + "m " + secs + "s ";
+  //noti.textContent = "Terrorist birthday in " + days + "d " + hours + "h " + mins + "m " + secs + "s ";
 
-  if (mins < 0){
-    clearInterval(interval);
-    noti.textContent = "happy bday raa Aditya :)";
+  //if (mins < 0){
+    //clearInterval(interval);
+   // noti.textContent = "happy bday raa Aditya :)";
   }
 }
